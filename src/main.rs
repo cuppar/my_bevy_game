172b54f538119fdaead130c080aa0536b96ea8fb
Cuppar <cuppar.hzy@gmail.com>
2024-02-05 23:41:42 +0800

    bevy getting started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use bevy::prelude::*;
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, greet_people);
+    }
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 fn main() {
-    println!("Hello, world2!");
+    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Cuppar He".to_string())));
+    commands.spawn((Person, Name("Xiao Wang".to_string())));
+    commands.spawn((Person, Name("Zhang Shang".to_string())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Cuppar He" {
+            name.0 = "Cuppar~".to_string();
+            break;
+        }
+    }
 }
