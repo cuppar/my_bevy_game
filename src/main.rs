6525448110e1913d65e19c4857b7f9b73e585451
Cuppar <cuppar.hzy@gmail.com>
2024-02-06 16:14:36 +0800

    s

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,27 @@
+mod camera;
+mod debug;
+mod movement;
+mod spaceship;
+
 use bevy::prelude::*;
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_systems(Startup, add_people)
-            .add_systems(Update, greet_people);
-    }
-}
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
+use camera::CameraPlugin;
+use debug::DebugPlugin;
+use movement::MovementPlugin;
+use spaceship::SpaceshipPlugin;
 
 fn main() {
-    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
+    App::new()
+        // Bevy built-ins.
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.0, 0.15)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 0.75,
+        })
+        .add_plugins(DefaultPlugins)
+        // User configured plugins.
+        .add_plugins(SpaceshipPlugin)
+        .add_plugins(MovementPlugin)
+        .add_plugins(CameraPlugin)
+        .add_plugins(DebugPlugin)
+        .run();
 }
-
-// fn hello_world() {
-//     println!("hello world!");
-// }
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Cuppar He".to_string())));
-    commands.spawn((Person, Name("Xiao Wang".to_string())));
-    commands.spawn((Person, Name("Zhang Shang".to_string())));
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
-// fn update_people(mut query: Query<&mut Name, With<Person>>) {
-//     for mut name in &mut query {
-//         if name.0 == "Cuppar He" {
-//             name.0 = "Cuppar~".to_string();
-//             break;
-//         }
-//     }
-// }
